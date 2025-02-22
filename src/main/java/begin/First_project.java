3c25ab37f83d2b5a17924d4e40ea01aa02eef90b
sammm29 <sm900552@gmail.com>
2025-02-22 19:45:36 +0530

    changed again

--- src/main/java/begin/First_project.java
+++ src/main/java/begin/First_project.java
@@ -4,7 +4,8 @@ public class First_project {
 public static void main(String [] args) {
 	try {
 		Class.forName("org.postgresql.Driver");
-		System.out.print("Driver Class is Uploaded Successfully");
+		System.out.print("Driver Class is Uploaded Successfully...!");
+		System.out.println("Doing the demo of git");
 	} catch (ClassNotFoundException e) {
 		e.printStackTrace();
 	}
